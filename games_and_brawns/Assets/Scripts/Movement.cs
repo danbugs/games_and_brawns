5307e5633756aeec8c14727ec414e71c4198fbc8
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-16 22:20:39 -0800

    concluded game without UI

---
+++ games_and_brawns/Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    Camera cam;
+    Vector3 current_position;
+    float timer = 0;
+    [SerializeField]
+    GameObject Manager;
+    List<float> jumps;
+    List<float> movement;
+    const float tick_speed = 0.1f;
+
+    void Start()
+    {
+        cam = Camera.main;
+        jumps = Manager.GetComponent<Processing>().jump_time_instances;
+        movement = Manager.GetComponent<Processing>().emg_values;
+    }
+
+    void Update()
+    {
+        timer += (Time.deltaTime) % 60;
+        current_position = cam.transform.position;
+
+        if(timer % tick_speed >=  0 - 0.01 && timer % tick_speed <= 0 + 0.01)
+        {
+            current_position.x += 1;
+            if (current_position.y > 0)
+            {
+                current_position.y -= 1;
+            }
+        }
+
+        if (jumps.Count > 0 && (timer >= jumps[0] - 0.01 && timer <= jumps[0] + 0.01))
+        {
+            //Debug.Log(timer + " = " + jumps[0]);
+            jumps.RemoveAt(0);
+            current_position.y += 2;
+        }
+
+        cam.transform.position = current_position;
+    }
+}
---
+++ games_and_brawns/Assets/Scripts/Processing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Processing : MonoBehaviour
+{
+
+    TextAsset file;
+    string[] lines;
+    public List<float> emg_values;
+    public List<float> jump_time_instances;
+    int findpeaks_helper = 0;
+
+    void Awake()
+    {
+        emg_values = new List<float>();
+        jump_time_instances = new List<float>();
+        //Debug.Log("LOADED");
+        file = (TextAsset)Resources.Load("data_converted");
+        lines = file.text.Split('\n');
+        for (float i = 0; i < lines.Length - 1; i++)
+        {
+            if (i >= 3)
+            {
+                string[] current_line = lines[(int) i].Split('\t');
+                emg_values.Add(float.Parse(current_line[5]));
+                if (emg_values.Count > 0 && emg_values[emg_values.Count - 1] > 0.3 && i - 1000 > findpeaks_helper)
+                {
+                    //Debug.Log("@" + i / 1000 + " found: " + emg_values[emg_values.Count - 1]);
+                    jump_time_instances.Add(i / 1000);
+                    findpeaks_helper = (int) i;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
