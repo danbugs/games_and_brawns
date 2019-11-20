8bc7242b53f626aa36397be819876d1f5f6bc120
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-20 15:18:27 -0800

    movement is clean now

--- games_and_brawns/Assets/Scripts/Movement.cs
+++ games_and_brawns/Assets/Scripts/Movement.cs
@@ -1,47 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 public class Movement : MonoBehaviour
 {
     Camera cam;
     Vector3 current_position;
-    float timer = 0;
     [SerializeField]
     GameObject Manager;
     List<float> jumps;
     List<float> movement;
-    const float tick_speed = 0.1f;
+    double start;
+    double timer;
+    double tick_speed;
 
     void Start()
     {
         cam = Camera.main;
         jumps = Manager.GetComponent<Processing>().jump_time_instances;
         movement = Manager.GetComponent<Processing>().emg_values;
+        start = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
     }
 
     void Update()
     {
-        timer += (Time.deltaTime) % 60;
+        tick_speed += Time.deltaTime;
+        timer = (double) (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - start)/1000;
+        while (tick_speed >= 0.1) {
+            Tick();
+            tick_speed -= 0.1;
+        }
+
+    }
+
+    void Tick()
+    {
+        Debug.Log(timer);
         current_position = cam.transform.position;
-
-        if(timer % tick_speed >=  0 - 0.01 && timer % tick_speed <= 0 + 0.01)
+        current_position.x += 1;
+        if (cam.transform.position.y > 0)
         {
-            current_position.x += 1;
-            if (current_position.y > 0)
-            {
-                current_position.y -= 1;
-            }
+            current_position.y--;
         }
-
-        if (jumps.Count > 0 && (timer >= jumps[0] - 0.01 && timer <= jumps[0] + 0.01))
+        if (jumps.Count > 0 && (timer >= jumps[0] - 0.05 && timer <= jumps[0] + 0.05))
         {
-            //Debug.Log(timer + " = " + jumps[0]);
             jumps.RemoveAt(0);
-            current_position.y += 2;
+            current_position.y += 3;
         }
-
         cam.transform.position = current_position;
     }
 }
