5045f24575d0da7413cfdbc8ea191bf6b57578f2
Dan Chiarlone <danilochiarlone@gmail.com>
2019-11-20 22:51:24 -0800

    concluded game without UI 2

--- games_and_brawns/Assets/Scripts/Processing.cs
+++ games_and_brawns/Assets/Scripts/Processing.cs
@@ -6,29 +6,31 @@ using System.IO;
 
 public class Processing : MonoBehaviour
 {
-
-    TextAsset file;
+    string file;
     string[] lines;
     public List<float> emg_values;
     public List<float> jump_time_instances;
     int findpeaks_helper = 0;
+    StreamReader input;
 
     void Awake()
     {
+        Time.timeScale = 1;
         emg_values = new List<float>();
         jump_time_instances = new List<float>();
-        //Debug.Log("LOADED");
-        file = (TextAsset)Resources.Load("data_converted");
-        lines = file.text.Split('\n');
+        //file = (TextAsset)Resources.Load("data_converted");
+        DirectoryInfo directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
+        input = File.OpenText(Path.Combine(Application.streamingAssetsPath, "data_converted.txt"));
+        file = input.ReadToEnd();
+        lines = file.Split('\n');
         for (float i = 0; i < lines.Length - 1; i++)
         {
             if (i >= 3)
             {
                 string[] current_line = lines[(int) i].Split('\t');
                 emg_values.Add(float.Parse(current_line[5]));
-                if (emg_values.Count > 0 && emg_values[emg_values.Count - 1] > 0.3 && i - 1000 > findpeaks_helper)
+                if (emg_values.Count > 0 && emg_values[emg_values.Count - 1] > 0.3 && i - 100 > findpeaks_helper)
                 {
-                    //Debug.Log("@" + i / 1000 + " found: " + emg_values[emg_values.Count - 1]);
                     jump_time_instances.Add(i / 1000);
                     findpeaks_helper = (int) i;
                 }
