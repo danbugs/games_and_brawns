8dc851d2386759148963d56bd9fcd0ae48dcbe0c
Matheus <mdasilva311@gmail.com>
2019-11-26 23:34:09 -0800

    Semi-complete game

--- games_and_brawns/Assets/Scripts/Movement.cs
+++ games_and_brawns/Assets/Scripts/Movement.cs
@@ -1,47 +1,63 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 public class Movement : MonoBehaviour
 {
     Camera cam;
     Vector3 current_position;
-    float timer = 0;
     [SerializeField]
     GameObject Manager;
     List<float> jumps;
     List<float> movement;
-    const float tick_speed = 0.1f;
+    double start;
+    double timer;
+    double tick_speed;
+    Animator anim;
 
     void Start()
     {
         cam = Camera.main;
         jumps = Manager.GetComponent<Processing>().jump_time_instances;
         movement = Manager.GetComponent<Processing>().emg_values;
+        start = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        anim = GameObject.FindGameObjectWithTag("player").GetComponent<Animator>();
     }
 
     void Update()
     {
-        timer += (Time.deltaTime) % 60;
+        tick_speed += Time.deltaTime;
+        timer = (double) (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - start)/1000;
         current_position = cam.transform.position;
-
-        if(timer % tick_speed >=  0 - 0.01 && timer % tick_speed <= 0 + 0.01)
+        if (Input.GetKeyDown("space"))
         {
-            current_position.x += 1;
-            if (current_position.y > 0)
-            {
-                current_position.y -= 1;
-            }
+            current_position.y += 3;
         }
+        while (tick_speed >= 0.1) {
+            Tick();
+            tick_speed -= 0.1;
+        }
+        cam.transform.position = current_position;
 
-        if (jumps.Count > 0 && (timer >= jumps[0] - 0.01 && timer <= jumps[0] + 0.01))
+    }
+
+    void Tick()
+    {
+        current_position.x += 1;
+        if (cam.transform.position.y > 0)
         {
-            //Debug.Log(timer + " = " + jumps[0]);
+            current_position.y--;
+        }
+        if (jumps.Count > 0 && (timer >= jumps[0] - 0.1 && timer <= jumps[0] + 0.1))
+        {
+
             jumps.RemoveAt(0);
-            current_position.y += 2;
+            current_position.y += 3;
         }
-
+        anim.SetFloat("jump", current_position.y);
         cam.transform.position = current_position;
     }
 }
---
+++ games_and_brawns/Assets/Scripts/VARIABLES.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class VARIABLES
+{
+    public static bool viz = true;
+}
